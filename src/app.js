cbeab34a5ad40ab887d946269833fb8086995732
Franco M. Cortinez <fcortnez@frba.utn.edu.ar>
2026-04-28 16:11:02 -0300

    Modifico app.js con el router de Turno

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import healthRouter from './modules/health/health.router.js';
 import medicosRouter from './modules/medicos/medicos.router.js';
+import turnoRouter from './modules/routes/turnoRouter.js';
 import router from './modules/routes/router.js';
 
 const app = express();
@@ -13,6 +14,6 @@ app.get('/', (req, res) => {
 
 app.use('/health', healthRouter);
 app.use('/medicos', medicosRouter);
-app.use(router);
+//router.use('/turno', turnoRouter);
 
 export default app;
